bbdb38a45bf7b9f96f5ecfecda89ef48ba17f5ec
Josue A. Oliveira <josueao.mobile@gmail.com>
2024-11-16 18:50:16 -0300

    Initial commit: Project started with basic structure and initial setup

---
+++ src/main/java/br/com/josueao/lab_graphql/LabGraphqlApplication.java
@@ -0,0 +1,13 @@
+package br.com.josueao.lab_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/josueao/lab_graphql/LabGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.josueao.lab_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
