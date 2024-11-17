3ac8f237e892e1b20b49669a6c2592d290dc282f
Josue A. Oliveira <josueao.mobile@gmail.com>
2024-11-17 12:20:10 -0300

    GraphQL: Implemented DataLoader for fetching Post comments

--- src/main/java/br/com/josueao/lab_graphql/ForumCrontroller.java
+++ src/main/java/br/com/josueao/lab_graphql/ForumCrontroller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -30,5 +31,10 @@ public class ForumCrontroller {
     @MutationMapping
     public Collection<Comment> createComment(@Argument String postId, @Argument String content) {
         return commentService.createComment(postId, content);
+    }
+
+    @SchemaMapping
+    public Collection<Comment> comments(Post post) {
+        return commentService.findByPost(post.id());
     }
 }
--- src/main/java/br/com/josueao/lab_graphql/LabGraphqlApplication.java
+++ src/main/java/br/com/josueao/lab_graphql/LabGraphqlApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LabGraphqlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LabGraphqlApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LabGraphqlApplication.class, args);
+    }
 
 }
